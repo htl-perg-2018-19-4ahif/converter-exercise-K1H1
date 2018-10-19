60affd2847e7147df86cd569f0d363bc33d1e8b2
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-10-19 11:45:44 +0200

    changed loop

--- convert.js
+++ convert.js
@@ -6,9 +6,11 @@ stringTo='to';
 
 
 
-if(process.argv.length === 4){
+if(process.argv.length === 6){
     toConvert=process.argv[1];
     for(let i=2;i<process.argv.length;i++){
+
+        //source
         switch(process.argv[2]){
             case 'm':
                 
