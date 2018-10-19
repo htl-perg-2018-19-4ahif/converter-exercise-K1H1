0795a8a09bf24dbb73163959d4559635d233fef8
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-10-19 11:42:07 +0200

    installed converter npm package

--- convert.js
+++ convert.js
@@ -1,1 +1,22 @@
 console.log('Not implemented yet!');
+
+let toConvert, sourceUnit, stringTo, targetUnit;
+stringTo='to';
+
+
+
+
+if(process.argv.length === 4){
+    toConvert=process.argv[1];
+    for(let i=2;i<process.argv.length;i++){
+        switch(process.argv[2]){
+            case 'm':
+                
+
+            case 'mm':
+
+        {
+    }
+
+
+}
