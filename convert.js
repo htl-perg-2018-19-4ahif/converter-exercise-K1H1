5f9aacefaf553753dfaa2d6820cc38e713237071
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-10-23 11:42:16 +0200

    -fixed error -->converter is working now (without the tests)

--- convert.js
+++ convert.js
@@ -1,10 +1,18 @@
-//console.log('Not implemented yet!');
-let convert = require('convert-units')
-let toConvert, sourceUnit, constString, targetUnit, solution;
-constString='to';
-        /*toConvert=process.argv[0];
-        sourceUnit=process.argv[2];
-        targetUnit=process.argv[3];*/
-        solution= convert(process.argv[1]).from(process.argv[2]).to(process.argv[4])
-        process.stdout.write('From' + toConvert+ ' '+sourceUnit+' '+constString+ ' '+solution+' '+targetUnit );
+let convert = require('convert-units');
+  /*toConvert=process.argv[0];
+                sourceUnit=process.argv[2];
+                targetUnit=process.argv[3];*/
+       
+        /*solution = convert(process.argv[3]).from(process.argv[4]).to(process.argv[6]);
+        //process.stdout.write('From' + process.argv[1] + ' ' + process.argv[2] + ' ' + constString + ' ' + solution + ' ' + process.argv[4]);
+        process.stdout.write(solution);*/
+
+        if(process.argv.length == 6 ){
+
+                console.log(process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + " " +  process.argv[5])
+        }else{
+
+                console.log("invalid input!");
+        }
+        
 
