d915f74e0e88f718795ab3ea321a89d37e093191
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-10-24 18:49:13 +0200

    -tried to pass all the unit tests

--- convert.js
+++ convert.js
@@ -1,18 +1,11 @@
 let convert = require('convert-units');
-  /*toConvert=process.argv[0];
-                sourceUnit=process.argv[2];
-                targetUnit=process.argv[3];*/
-       
-        /*solution = convert(process.argv[3]).from(process.argv[4]).to(process.argv[6]);
-        //process.stdout.write('From' + process.argv[1] + ' ' + process.argv[2] + ' ' + constString + ' ' + solution + ' ' + process.argv[4]);
-        process.stdout.write(solution);*/
+if (process.argv.length == 6 || process.argv[3] != 'm' || process.argv[3] != 'cm' || process.argv[3] != 'mm' || process.argv[3] != 'kg' || process.argv[3] != 'kg'
+|| process.argv[5] != 'm' || process.argv[5] != 'cm' || process.argv[5] != 'mm') {
+        //process.stdout.write((process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + " " + process.argv[5]);
+        console.log(process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + " " + process.argv[5])
+} else {
+        //process.stdout.write('Invalid input! -> Check number of arguments!');
+        console.log('Invalid input!');
+}
 
-        if(process.argv.length == 6 ){
-
-                console.log(process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]) + " " +  process.argv[5])
-        }else{
-
-                console.log("invalid input!");
-        }
-        
 
